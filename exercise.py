96c468c813f3fa2e4373395d0cf663ce96838ae0
MKline22 <78701496+MKline22@users.noreply.github.com>
2021-03-07 22:42:48 -0500

    added user interface

--- exercise.py
+++ exercise.py
@@ -1,3 +1,6 @@
+print("Please choose one of the following exercises:\n ")
+print("1) Bench press\n 2) Push ups\n 3) Dead lists\n 4) Jumping jax\n 5) Squats")
+usrinput = input("Enter choice: ")
 workouts = open("workouts.tsv")
 
 workout_lists = []
